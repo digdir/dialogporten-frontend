0f303bb69c21d1b04ee3fb6e18a7b227739a4920
Alexander Digdir <DigDir@Alex-MBP-M1Pro.local>
2023-09-12 14:35:05 +0200

    Added css module types

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
 import React from "react";
+import classes from "./App.module.css";
 
-export const App = () => <h1>Hello World</h1>;
+export const App = () => <h1 className={classes.heading}>Hello World</h1>;
