418873811e17f6b0f69174bd25fe2425d2c88e3d
Alexander Digdir <DigDir@Alex-MBP-M1Pro.local>
2023-09-12 14:08:49 +0200

    feat: Basic yarn setup

---
+++ webpack.config.dev.js
@@ -0,0 +1,49 @@
+const commonConfig = require("./webpack.common");
+const path = require("path");
+
+module.exports = {
+  ...commonConfig,
+  mode: "development",
+  devtool: "inline-source-map",
+  performance: {
+    hints: "warning",
+  },
+  optimization: {
+    chunkIds: "natural",
+  },
+  module: {
+    rules: [
+      ...commonConfig.module.rules,
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+      {
+        test: /\.([jt]sx?)?$/,
+        use: "swc-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [...commonConfig.plugins],
+  devServer: {
+    hot: true,
+    static: path.join(__dirname, "/src/public"),
+    allowedHosts: "all",
+    port: 3000,
+    historyApiFallback: true,
+    client: {
+      overlay: {
+        errors: false,
+        warnings: false,
+      },
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      // prettier-ignore
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+    },
+  },
+};
