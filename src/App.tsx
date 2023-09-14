c67ec69b98cefda30b79806e42b9c335e2ff54e4
Alexander Midteide <144693023+alexdigdir@users.noreply.github.com>
2023-09-14 14:11:57 +0200

    Feat/setup eslint and prettier (#64)
    * Updated Readme file
    
    * FEAT/#11063 Added Eslint and prettier
    
    ---------
    
    Co-authored-by: Alexander Digdir <DigDir@Alex-MBP-M1Pro.local>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
-import classes from "./App.module.css";
+import React from 'react';
+import classes from './App.module.css';
 
 export const App = () => <h1 className={classes.heading}>Hello World</h1>;
