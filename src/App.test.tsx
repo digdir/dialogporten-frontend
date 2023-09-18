f71c2c27b8ae736925c7b4737bb8be2668ffdec6
Alexander Midteide <144693023+alexdigdir@users.noreply.github.com>
2023-09-18 13:28:03 +0200

    Added React Router to project (#69)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { App } from './App';
 
-test('renders Hello World text', () => {
-  render(<App title='Hello World' />);
+test('renders PageLayout text', () => {
+  render(<App />);
   const linkElement = screen.getByRole('heading', {
-    name: 'Hello World',
+    name: 'PageLayout',
     level: 1,
   });
   expect(linkElement).toBeInTheDocument();
-});
-
-test('clicking button executes onClick function', async () => {
-  const onClickMock = jest.fn();
-  render(<App title='Hello World' onClick={onClickMock} />);
-  await userEvent.click(screen.getByRole('button', { name: 'Send inn' }));
-  expect(onClickMock).toHaveBeenCalled();
-  screen.getByLabelText('Hallo');
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
-import classes from './App.module.css';
+import { RouterProvider } from 'react-router-dom';
+import { routes } from './routes/routes';
 
-interface AppProps {
-  title?: string;
-  onClick?: () => void;
-}
-
-export const App = ({ title, onClick }: AppProps) => {
-  return (
-    <div>
-      <h1 className={classes.heading}>{title || "Hello"}</h1>
-      <button onClick={onClick}>Send inn</button>
-      <label htmlFor="hallo">Hallo</label>
-      <input type="text" id="hallo" />
-    </div>
-  );
+export const App = () => {
+  return <RouterProvider router={routes} />;
 };
