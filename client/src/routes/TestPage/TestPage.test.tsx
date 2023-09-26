5bfa0d1884f605c42d5c2326d6b773f1ad012657
Alexander Midteide <alexander.midteide@digdir.no>
2023-09-26 11:38:42 +0200

    First attempt

---
+++ client/src/routes/TestPage/TestPage.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'; // Use MemoryRouter to simulate routing
+import { TestPage } from './TestPage';
+
+test('renders TestPage component with a link to the root', () => {
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <TestPage />
+    </MemoryRouter>
+  );
+
+  // Check if the component's content is present
+  expect(screen.getByText('Test page')).toBeInTheDocument();
+
+  // Check if the link to the root is present with the correct href
+  const rootLink = screen.getByRole('link', { name: 'Go to Root' });
+  expect(rootLink).toBeInTheDocument();
+  expect(rootLink).toHaveAttribute('href', '/');
+});
