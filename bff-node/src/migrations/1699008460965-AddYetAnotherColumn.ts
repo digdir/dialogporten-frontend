b2731e3bab0ec83f5b46fd886b8e13c2f6f946bf
Alexander Midteide <alexander.midteide@digdir.no>
2023-11-03 12:14:38 +0100

    Running migration

--- bff-node/src/migrations/1699008460965-AddYetAnotherColumn.ts
+++ bff-node/src/migrations/1699008460965-AddYetAnotherColumn.ts
@@ -1,16 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddYetAnotherColumn1699008460965 implements MigrationInterface {
-    name = 'AddYetAnotherColumn1699008460965'
+  name = 'AddYetAnotherColumn1699008460965';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "person" ADD "anotherColumn" integer`);
-        await queryRunner.query(`ALTER TABLE "person" ADD "yetAnotherColumn" integer`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "person" ADD "yetAnotherColumn" integer`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "person" DROP COLUMN "yetAnotherColumn"`);
-        await queryRunner.query(`ALTER TABLE "person" DROP COLUMN "anotherColumn"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "person" DROP COLUMN "yetAnotherColumn"`);
+  }
 }
