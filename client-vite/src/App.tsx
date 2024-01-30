02c50723c1216eaf9628e3f0ef8cc2d2ae3b5010
Sean Scully <seanscully@Sean-sin-MacBook-Pro.local>
2024-01-30 13:50:46 +0100

    Adding new init of client  with vite, typesript and css modules enabled

---
+++ client-vite/src/App.tsx
@@ -0,0 +1,11 @@
+import appStyles from './app.module.css'
+
+function App() {
+  return (
+    <section className={appStyles.app}>
+      <h1>Hello, arbeidsflate!</h1>
+    </section>
+  )
+}
+
+export default App
