5838115bbd0c1f38e2502dd549f6776425528636
Sean Scully <seanscully@Sean-sin-MacBook-Pro.local>
2024-01-31 15:17:58 +0100

    Add msw with simple endpoint example, enabled in dev

--- client-vite/src/App.tsx
+++ client-vite/src/App.tsx
@@ -1,11 +1,22 @@
-import appStyles from './app.module.css'
+import { useEffect, useState } from 'react';
+import appStyles from './app.module.css';
 
 function App() {
-  return (
-    <section className={appStyles.app} data-testid="app">
-      <h1>Hello, arbeidsflate!</h1>
-    </section>
-  )
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [username, setUsername] = useState<string>('');
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('/user')
+      .then((resp) => resp.json())
+      .then((user) => setUsername(user.name))
+      .finally(() => setIsLoading(false));
+
+  }, []);
+
+  return (<section className={appStyles.app} data-testid="app">
+    {isLoading ? <p>Is loading</p> : <h1>Hello, {username}!</h1>}
+  </section>);
 }
 
-export default App
+export default App;
