53ff6f9041cff8a1f08f82fae178f4ec316345f7
Sean Scully <seanscully@Sean-sin-MacBook-Pro.local>
2024-01-31 15:17:58 +0100

    Add msw with simple endpoint example, enabled in dev

--- client-vite/src/main.tsx
+++ client-vite/src/main.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+async function enableMocking() {
+  if (import.meta.env.MODE === 'development') {
+    const { worker } = await import('./mocks/browser');
+    return worker.start();
+  }
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>);
+});
+
---
+++ client-vite/src/mocks/handlers.ts
@@ -0,0 +1,5 @@
+import { http, HttpResponse } from 'msw';
+
+export const handlers = [http.get('/user', () => {
+  return HttpResponse.json({ name: 'John Doe', scopes: [] });
+})];
