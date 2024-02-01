a363a60c7ddd34dd8df7d59c6fc9efdcf28a7901
Sean Scully <seanscully@Sean-sin-MacBook-Pro.local>
2024-02-01 15:50:59 +0100

    feature: add react-query (#167)

--- client-vite/vitest.config.ts
+++ client-vite/vitest.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// https://vitest.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: "jsdom"
+    environment: "jsdom",
+    globals: true,
+    css: {
+      modules: {
+        classNameStrategy: 'non-scoped',
+      },
+    },
   }
 })
