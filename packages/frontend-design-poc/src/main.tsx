af32c0792b5ce249209937ffbf0e3e98f87b61ca
Sean Scully <seanscully@Sean-sin-MacBook-Pro.local>
2024-02-05 13:46:36 +0100

    Create skeleton for component hierarchy with page layout (#184)

--- packages/frontend-design-poc/src/main.tsx
+++ packages/frontend-design-poc/src/main.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter } from "react-router-dom";
 
 async function enableMocking() {
-  if (import.meta.env.MODE === 'development') {
-    const { worker } = await import('./mocks/browser');
+  if (import.meta.env.MODE === "development") {
+    const { worker } = await import("./mocks/browser");
     return worker.start();
   }
 }
 
-enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode>
-    <App />
-  </React.StrictMode>);
-});
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+const queryClient = new QueryClient();
 
+enableMocking().then(() => {
+  root.render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </React.StrictMode>,
+  );
+});
