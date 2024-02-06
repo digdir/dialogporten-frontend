7d1f05bf73d8e2066233035d8cb1ae8c3d498784
Sean Erik Scully <sean.erik.scully@gmail.com>
2024-02-06 12:51:45 +0100

    feature: add react-i18next with ICU (#183) (#189)
    * feature: add react-i18next with ICU (#183)

--- packages/frontend-design-poc/src/components/HelloWorld/HelloWorld.tsx
+++ packages/frontend-design-poc/src/components/HelloWorld/HelloWorld.tsx
@@ -1,12 +1,18 @@
 import styles from "./helloWorld.module.css";
 import { useQuery } from "react-query";
 import { getUser } from "../../api/queries.ts";
+import { useTranslation } from "react-i18next";
 
 export const HelloWorld = () => {
   const { isLoading, data } = useQuery("user", getUser);
+  const { t } = useTranslation();
   return (
     <section className={styles.helloWorld}>
-      {isLoading ? <span>Loading ...</span> : <h1>Hello, {data?.name}!</h1>}
+      {isLoading ? (
+        <span>Loading ...</span>
+      ) : (
+        <h1>{t("example.hello", { person: data?.name })}!</h1>
+      )}
     </section>
   );
 };
--- packages/frontend-design-poc/src/main.tsx
+++ packages/frontend-design-poc/src/main.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
+import "./i18n/";
+
+import App from "./App.tsx";
 
 async function enableMocking() {
   if (import.meta.env.MODE === "development") {
