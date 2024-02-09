17e331c9334278fe6fa6c0bd7bc32cd31b6d5e6f
Sean Scully <seanscully@Sean-sin-MacBook-Pro.local>
2024-02-09 14:56:29 +0100

    feat: Add InboxItemPage component

---
+++ packages/frontend-design-poc/src/pages/InboxItemPage/InboxItemPage.tsx
@@ -0,0 +1,8 @@
+import styles from "./inboxItemPage.module.css";
+
+export const InboxItemPage = () => {
+  return (
+    <div className={styles.itemInboxPage}>
+    </div>
+  );
+};
---
+++ packages/frontend-design-poc/src/pages/InboxItemPage/index.ts
@@ -0,0 +1,1 @@
+export { InboxItemPage } from "./InboxItemPage.tsx";
