bc1dd1a8c4321fceed71ad1a5f2a8e9d9a660f05
Sean Scully <seanscully@Sean-sin-MBP.home>
2024-02-19 10:09:01 +0100

    adds missing import to PageLayout

--- packages/frontend-design-poc/src/pages/PageLayout/PageLayout.tsx
+++ packages/frontend-design-poc/src/pages/PageLayout/PageLayout.tsx
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 import { Outlet } from 'react-router-dom';
 import { Header } from '../../components';
 import styles from './pageLayout.module.css';
 import { Footer, Sidebar } from '../../components';
+=======
+import { Outlet } from "react-router-dom";
+import { Header, Footer } from '../../components';
+
+import styles from "./pageLayout.module.css";
+>>>>>>> 3f25efa (adds missing import to PageLayout)
 
 export const PageLayout = () => {
 	return (
