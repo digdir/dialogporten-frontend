99a0ccda6f39fd29f993acd36bbed9567a819e87
Sean Scully <seanscully@Sean-sin-MBP.home>
2024-02-19 09:51:38 +0100

    feat: Add Footer component to PageLayout

--- packages/storybook/src/stories/Footer/footer.stories.ts
+++ packages/storybook/src/stories/Footer/footer.stories.ts
@@ -1,25 +1,14 @@
-import { Header } from 'frontend-design-poc';
-import { Meta, StoryObj } from '@storybook/react';
-import { withRouter } from 'storybook-addon-react-router-v6';
+import { Footer } from "frontend-design-poc";
+import { Meta, StoryObj } from "@storybook/react";
+import { withRouter } from "storybook-addon-react-router-v6";
 
 export default {
-	title: 'Components/Header',
-	component: Header,
-	decorators: [withRouter],
-	parameters: {
-		layout: 'fullscreen',
-	},
-} as Meta<typeof Header>;
+  title: "Components/Footer",
+  component: Footer,
+  decorators: [withRouter],
+  parameters: {
+    layout: "fullscreen",
+  },
+} as Meta<typeof Footer>;
 
-export const Default: StoryObj<typeof Header> = {
-	args: {
-		name: 'Ola Nordmann',
-	},
-};
-
-export const WithCompanyName: StoryObj<typeof Header> = {
-	args: {
-		name: 'Ola Nordmann',
-		companyName: 'Aker Solutions AS',
-	},
-};
+export const Default: StoryObj<typeof Footer> = {};
