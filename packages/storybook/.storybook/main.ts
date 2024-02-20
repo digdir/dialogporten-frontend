c67cc62a1362956554783010ec004750dac38472
Sean Scully <seanscully@Sean-sin-MBP.home>
2024-02-20 16:02:58 +0100

    feat: Update Storybook config and add accessibility rules

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -1,20 +1,21 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'storybook-addon-react-router-v6',
-		'@storybook/addon-interactions',
-		'storybook-react-i18next'
-	],
-	framework: {
-		name: '@storybook/react-vite',
-		options: {},
-	},
-	docs: {
-		autodocs: 'tag',
-	},
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-addon-react-router-v6",
+    "@storybook/addon-interactions",
+    "storybook-react-i18next",
+    "@storybook/addon-a11y",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
 };
 export default config;
