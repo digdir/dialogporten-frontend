e312e3d54100febad2b101eb9d5b617daa619376
Sean Erik Scully <sean.erik.scully@gmail.com>
2024-02-21 13:00:10 +0100

    feat: Update Storybook config and add accessibility rules (#234)

--- packages/storybook/.storybook/preview.tsx
+++ packages/storybook/.storybook/preview.tsx
@@ -4,8 +4,30 @@ import customTheme from './customTheme';
 import i18n from 'i18next';
 import { I18nextProvider } from 'react-i18next';
 import { nbResources } from 'frontend-design-poc';
+import { Rule, getRules } from "axe-core";
+import { A11yParameters } from '@storybook/addon-a11y';
 
 import './i18next'
+
+const enabledTags = [
+  "wcag2a",
+  "wcag2aa",
+  "wcag21a",
+  "wcag21aa",
+  "wcag22aa",
+  "best-practice",
+];
+
+const enabledRules: Rule[] = getRules(enabledTags).map((ruleMetadata) => ({
+  id: ruleMetadata.ruleId,
+  enabled: true,
+}));
+
+const a11y: A11yParameters = {
+  config: {
+    rules: enabledRules,
+  },
+};
 
 const preview: Preview = {
   globals: {
@@ -25,6 +47,7 @@ const preview: Preview = {
   ],
   parameters: {
     i18n,
+    a11y,
     actions: { argTypesRegex: '^on[A-Z].*' }, docs: {
       theme: customTheme
     }, controls: {
