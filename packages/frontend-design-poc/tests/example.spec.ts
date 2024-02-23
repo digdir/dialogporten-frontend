cb703da1c0864b6014b4378c44d30200052dc3b2
Sean Erik Scully <sean.erik.scully@gmail.com>
2024-02-23 16:29:48 +0100

    feat: Add Playwright test workflow and configuration (#214) (#241)

---
+++ packages/frontend-design-poc/tests/example.spec.ts
@@ -0,0 +1,19 @@
+import { expect, test } from "@playwright/test";
+test.beforeEach(async ({ page }) => {
+  await page.goto(
+    process.env.PLAYWRIGHT_TEST_BASE_URL || "http://localhost:5173/",
+  );
+});
+test.describe('Smoke test', () => {
+  test('should show header, aside, main and footer', async ({ page }) => {
+    const main = page.locator('main');
+    const aside = page.locator('aside')
+    const footer = page.locator('footer');
+    const header = page.locator('header');
+
+    await expect(main).toBeVisible()
+    await expect(aside).toBeVisible()
+    await expect(footer).toBeVisible()
+    await expect(header).toBeVisible()
+  });
+});
--- packages/frontend-design-poc/vitest.config.ts
+++ packages/frontend-design-poc/vitest.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   test: {
+    exclude: ['node_modules', 'tests'], // tests for Playwright
     environment: "jsdom",
     globals: true,
     css: {
