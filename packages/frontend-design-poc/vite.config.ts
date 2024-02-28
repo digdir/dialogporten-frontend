b86c412e630020a61df73514920ba0c96e46c26f
Sean Scully <seanscully@Sean-sin-MacBook-Pro.local>
2024-02-28 18:26:58 +0100

    refactor: Update vite.config.ts for better readability

--- packages/frontend-design-poc/vite.config.ts
+++ packages/frontend-design-poc/vite.config.ts
@@ -1,19 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default () => {
-	if(process.env.PORT) {
-		return defineConfig({
-			plugins: [react()],
-
-			server: {
-				port: parseInt(process.env.PORT),
-			},
-		});
-	}
-
-	return defineConfig({
-		plugins: [react()],
-	});
-}
+  return defineConfig({
+    plugins: [react()],
+    ...(process.env.PORT && {
+      server: {
+        port: parseInt(process.env.PORT),
+      },
+    }),
+  });
+};
