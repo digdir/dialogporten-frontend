4657bb2b9bb300f629823e0e2040e32f38ac7225
Alexander Midteide <144693023+alexdigdir@users.noreply.github.com>
2024-02-28 11:40:31 +0100

    Added flags for App insights and HTTPS

--- packages/bff/src/util/sessionUtils.ts
+++ packages/bff/src/util/sessionUtils.ts
@@ -1,15 +1,12 @@
 import { CookieOptions } from 'express';
 import session from 'express-session';
 import '../config/env';
-import { isLocal } from '..';
 
 export const setCookie = (res: any, value: string) => {
   const cookieName = process.env.COOKIE_NAME || 'cookieName';
   const options: CookieOptions = {
-    // maxAge: 1000 * 60 * 5, // Cookie will expire in 5 min
-    // maxAge not set means cookie will be removed when browser is closed
     httpOnly: true, // Cookie not accessible via client-side script
-    secure: process.env.IS_LOCAL === 'true' ? false : true, // Cookie will be sent only over HTTPS if set to true
+    secure: process.env.ENABLE_HTTPS === 'true' ? true : false, // Cookie will be sent only over HTTPS if set to true
   };
   res.cookie(cookieName, value, options);
 };
@@ -32,5 +29,5 @@ export const sessionMiddleware = session({
   secret: process.env.SESSION_SECRET || 'SecretHere',
   resave: false,
   saveUninitialized: false,
-  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000, secure: isLocal ? false : true },
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000, secure: process.env.ENABLE_HTTPS ? true : false },
 });
