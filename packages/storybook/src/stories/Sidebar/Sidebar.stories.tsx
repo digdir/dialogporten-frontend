53068923fbeadf828de8b62fad88aa4b9b155127
Sean Scully <seanscully@O-jnonseid-X250.domain.local>
2024-02-29 10:58:59 +0100

    chore: Remove unused imports and update storybook titles

--- packages/storybook/src/stories/Sidebar/Sidebar.stories.tsx
+++ packages/storybook/src/stories/Sidebar/Sidebar.stories.tsx
@@ -1,96 +1,96 @@
 import type { Meta } from '@storybook/react';
+import { HorizontalLine, Sidebar, SidebarItem } from 'frontend-design-poc';
 import { withRouter } from 'storybook-addon-react-router-v6';
-import { Sidebar, SidebarItem, HorizontalLine } from 'frontend-design-poc';
 
 import {
-	FileTextIcon,
-	FileCheckmarkIcon,
-	FolderMinusIcon,
-	TrashIcon,
-	CogIcon,
-	MagnifyingGlassIcon,
-	PlusIcon,
-	InboxFillIcon,
+  CogIcon,
+  FileCheckmarkIcon,
+  FileTextIcon,
+  FolderMinusIcon,
+  InboxFillIcon,
+  MagnifyingGlassIcon,
+  PlusIcon,
+  TrashIcon,
 } from '@navikt/aksel-icons';
 
 const meta = {
-	title: 'Example/Sidebar',
-	component: Sidebar,
-	decorators: [withRouter],
-	tags: ['autodocs'],
+  title: 'Components/Sidebar',
+  component: Sidebar,
+  decorators: [withRouter],
+  tags: ['autodocs'],
 } satisfies Meta<typeof Sidebar>;
 
 export default meta;
 const SidebarSimpleExamplePerson = () => {
-	return (
-		<Sidebar>
-			<SidebarItem
-				displayText="Innboks"
-				label="Trykk her for å gå til innboks"
-				icon={<InboxFillIcon />}
-				count={3}
-				path="/innboks"
-				isInbox
-			/>
-			<HorizontalLine />
-			<SidebarItem
-				displayText="Nytt skjema"
-				label="Trykk her for å gå til utboks"
-				icon={<PlusIcon />}
-				path="/Nytt"
-				isButton
-			/>
-			<SidebarItem
-				displayText="Utkast"
-				label="Trykk her for å gå til Utkast"
-				icon={<FileTextIcon />}
-				count={8}
-				path="/Utkast"
-			/>
-			<SidebarItem
-				displayText="Sendt"
-				label="Trykk her for å gå til Sendt"
-				icon={<FileCheckmarkIcon />}
-				count={8}
-				path="/Sendt"
-			/>
-			<HorizontalLine />
-			<SidebarItem
-				displayText="Arkiv"
-				label="Trykk her for å gå til Arkiv"
-				icon={<FolderMinusIcon />}
-				count={8}
-				path="/Arkiv"
-			/>
-			<SidebarItem
-				displayText="Slettet"
-				label="Trykk her for å gå til Slettet"
-				icon={<TrashIcon />}
-				count={8}
-				path="/Slettet"
-			/>
-			<HorizontalLine />
-			<SidebarItem
-				displayText="Lagrede søk"
-				label="Trykk her for å gå til utboks"
-				icon={<MagnifyingGlassIcon />}
-				count={8}
-				path="/Lagrede"
-				type="secondary"
-			/>
-			<SidebarItem
-				displayText="Innstillinger"
-				label="Trykk her for å gå til utboks"
-				icon={<CogIcon />}
-				path="/innstillinger"
-				type="secondary"
-			/>
-		</Sidebar>
-	);
+  return (
+    <Sidebar>
+      <SidebarItem
+        displayText="Innboks"
+        label="Trykk her for å gå til innboks"
+        icon={<InboxFillIcon />}
+        count={3}
+        path="/innboks"
+        isInbox
+      />
+      <HorizontalLine />
+      <SidebarItem
+        displayText="Nytt skjema"
+        label="Trykk her for å gå til utboks"
+        icon={<PlusIcon />}
+        path="/Nytt"
+        isButton
+      />
+      <SidebarItem
+        displayText="Utkast"
+        label="Trykk her for å gå til Utkast"
+        icon={<FileTextIcon />}
+        count={8}
+        path="/Utkast"
+      />
+      <SidebarItem
+        displayText="Sendt"
+        label="Trykk her for å gå til Sendt"
+        icon={<FileCheckmarkIcon />}
+        count={8}
+        path="/Sendt"
+      />
+      <HorizontalLine />
+      <SidebarItem
+        displayText="Arkiv"
+        label="Trykk her for å gå til Arkiv"
+        icon={<FolderMinusIcon />}
+        count={8}
+        path="/Arkiv"
+      />
+      <SidebarItem
+        displayText="Slettet"
+        label="Trykk her for å gå til Slettet"
+        icon={<TrashIcon />}
+        count={8}
+        path="/Slettet"
+      />
+      <HorizontalLine />
+      <SidebarItem
+        displayText="Lagrede søk"
+        label="Trykk her for å gå til utboks"
+        icon={<MagnifyingGlassIcon />}
+        count={8}
+        path="/Lagrede"
+        type="secondary"
+      />
+      <SidebarItem
+        displayText="Innstillinger"
+        label="Trykk her for å gå til utboks"
+        icon={<CogIcon />}
+        path="/innstillinger"
+        type="secondary"
+      />
+    </Sidebar>
+  );
 };
 
 const SidebarSimpleExampleCompany = () => {
-	return <Sidebar />;
+  return <Sidebar />;
 };
 
 export const simpleDesktopExamplePerson: () => JSX.Element = () => <SidebarSimpleExamplePerson />;
