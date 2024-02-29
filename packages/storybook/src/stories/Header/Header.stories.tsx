73519be0692131dc6e1bafc729caa3cc6f7bd595
Sean Erik Scully <sean.erik.scully@gmail.com>
2024-02-29 13:23:17 +0100

    chore: Remove unused imports and update storybook titles (#267)
    Co-authored-by: Sean Scully <seanscully@O-jnonseid-X250.domain.local>

--- packages/storybook/src/stories/Header/Header.stories.tsx
+++ packages/storybook/src/stories/Header/Header.stories.tsx
@@ -1,12 +1,12 @@
-import { Header } from '../../../../frontend-design-poc/src/components/Header';
 import { Meta } from '@storybook/react';
+import { Header } from 'frontend-design-poc';
 import { withRouter } from 'storybook-addon-react-router-v6';
 
 const meta = {
-	title: 'Example/Header',
-	component: Header,
-	decorators: [withRouter],
-	tags: ['autodocs'],
+  title: 'Components/Header',
+  component: Header,
+  decorators: [withRouter],
+  tags: ['autodocs'],
 } satisfies Meta<typeof Header>;
 
 export default meta;
@@ -14,5 +14,5 @@ export default meta;
 export const withNameOnly: () => JSX.Element = () => <Header name="Ola Nordmann" />;
 
 export const withNameAndCompany: () => JSX.Element = () => (
-	<Header name="Ola Nordmann" companyName="Aker Solutions AS" />
+  <Header name="Ola Nordmann" companyName="Aker Solutions AS" />
 );
