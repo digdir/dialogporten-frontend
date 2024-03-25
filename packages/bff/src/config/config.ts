4b589a008fd39922aa24113344a04f87c445f62d
Are Almaas <arealmaas@gmail.com>
2024-03-25 16:36:44 +0100

    cleanup

--- packages/bff/src/config/config.ts
+++ packages/bff/src/config/config.ts
@@ -1,12 +1,10 @@
 import z from 'zod';
 
-// Setup types and default values for enviroment variables
 const envVariables = z.object({
   DB_CONNECTION_STRING: z.string(),
   APPLICATIONINSIGHTS_CONNECTION_STRING: z.string().optional(),
 });
 
-// Parse `process.env`
 const env = envVariables.parse(process.env);
 
 export default {
