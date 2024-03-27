a54b9c40750531e0b895f709890d7c3d5c916e04
Sean Scully <seanscully@Sean-sin-MacBook-Pro.local>
2024-03-27 13:00:52 +0100

    fix: Update route path for authentication router

--- packages/bff/src/routes/index.ts
+++ packages/bff/src/routes/index.ts
@@ -2,6 +2,6 @@ import express from 'express';
 import { authenticationRouter } from './authenticationRouter';
 
 const routes = express.Router();
-routes.use('/', authenticationRouter);
+routes.use('/auth', authenticationRouter);
 
 export { routes };
