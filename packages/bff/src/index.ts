a74824b7c4d01c943f36df35082d2eee528986c9
Sean Scully <seanscully@Sean-sin-MacBook-Pro.local>
2024-03-27 12:57:24 +0100

    fix

--- packages/bff/src/index.ts
+++ packages/bff/src/index.ts
@@ -1,15 +1,15 @@
-export const bffVersion = process.env.GIT_SHA || "v6.1.5";
-import express, { Express } from "express";
-import "reflect-metadata";
-import { DataSource, Repository } from "typeorm";
-import { startLivenessProbe, startReadinessProbe } from "./routes/HealthProbes";
-import cookieParser from "cookie-parser";
-import passport from "passport";
-import { Profile } from "./entities/Profile";
-import { SessionData } from "./entities/SessionData";
-import { initPassport } from "./oidc/passport";
-import { sessionMiddleware } from "./oidc/sessionUtils";
-import cors from "cors";
+export const bffVersion = process.env.GIT_SHA || 'v6.1.5';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express, { Express } from 'express';
+import passport from 'passport';
+import 'reflect-metadata';
+import { DataSource, Repository } from 'typeorm';
+import { Profile } from './entities/Profile';
+import { SessionData } from './entities/SessionData';
+import { initPassport } from './oidc/passport';
+import { sessionMiddleware } from './oidc/sessionUtils';
+import { startLivenessProbe, startReadinessProbe } from './routes/HealthProbes';
 
 const port = process.env.PORT || 3000;
 
@@ -30,18 +30,14 @@ declare module 'express-session' {
 const main = async (): Promise<void> => {
   startLivenessProbe(app, startTimeStamp);
   app.listen(port, () => {
-    console.log(
-      `BFF: ⚡️[server]: Server ${bffVersion} is running on PORT: ${port}`,
-    );
+    console.log(`BFF: ⚡️[server]: Server ${bffVersion} is running on PORT: ${port}`);
   });
 
   // ************ INITIALIZE APPLICATION INSIGHTS ************
   if (isAppInsightsEnabled) {
     const { initAppInsights } = await import('./ApplicationInsightsInit');
     await initAppInsights();
-    console.log(
-      `Starting BFF ${bffVersion} with GIT SHA: ${process.env.GIT_SHA}.`,
-    );
+    console.log(`Starting BFF ${bffVersion} with GIT SHA: ${process.env.GIT_SHA}.`);
   }
 
   // ************ CONNECT TO DB ************
@@ -64,10 +60,10 @@ const main = async (): Promise<void> => {
 
   /* CORS configuration for local env, needs to be applied before routes are defined */
   const corsOptions = {
-    origin: [ 'http://frontend-design-poc.localhost', 'http://localhost:3000' ],
+    origin: ['http://frontend-design-poc.localhost', 'http://localhost:3000'],
     credentials: true,
-    methods: "GET, POST, PATCH, DELETE, PUT",
-    allowedHeaders: "Content-Type, Authorization",
+    methods: 'GET, POST, PATCH, DELETE, PUT',
+    allowedHeaders: 'Content-Type, Authorization',
     preflightContinue: true,
   };
 
@@ -79,12 +75,10 @@ const main = async (): Promise<void> => {
   await initPassport();
 
   try {
-    const { routes: authRoutes } = await import("./routes");
+    const { routes: authRoutes } = await import('./routes');
 
-    app.get("/", (req, res) => {
-      res.send(
-        `BFF: ⚡️[server]: Server ${bffVersion} is running on PORT: ${port}`,
-      );
+    app.get('/', (req, res) => {
+      res.send(`BFF: ⚡️[server]: Server ${bffVersion} is running on PORT: ${port}`);
     });
 
     app.use(express.json());
@@ -97,4 +91,4 @@ const main = async (): Promise<void> => {
   }
 };
 
-main();
+void main();
