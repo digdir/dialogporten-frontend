6a2f53e33338fedeeeda0c4ef73570ff3a03c2c3
Frank Lyder Bredland <fl@nyggen.com>
2024-04-30 12:04:26 +0200

    feat: codegen for frontend based on GraphQL types in the BFF (#541)

---
+++ packages/bff-types-generated/schema.ts
@@ -0,0 +1,3 @@
+import { schema } from 'bff';
+
+export default schema;
---
+++ packages/bff/src/app.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env -S node --no-warnings=ExperimentalWarning --loader ts-node/esm
+import 'reflect-metadata';
+import config from './config.ts';
+import { runMigrationApp } from './run-migration.ts';
+import startServer from './server.ts';
+
+try {
+  if (config.migrationRun) {
+    await runMigrationApp();
+  } else {
+    await startServer();
+  }
+} catch (error) {
+  console.error(error);
+}
