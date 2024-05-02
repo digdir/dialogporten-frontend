727760cda3a813fa7c9a0a25a913d52d132a1691
Sean Erik Scully <sean.erik.scully@gmail.com>
2024-05-02 22:18:00 +0200

    feat: add graphql-request for fetching data with example (#543)

--- packages/bff-types-generated/codegen.ts
+++ packages/bff-types-generated/codegen.ts
@@ -3,14 +3,19 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   schema: './schema.ts',
   require: ['ts-node/register'],
-
-	documents: 'queries/**/*.graphql',
-
+  documents: 'queries/**/*.graphql',
   generates: {
     './generated/sdk.ts': {
-			plugins: ['typescript', 'typescript-operations', 'typescript-graphql-request'],
+      plugins: [{
+        'add': {
+          content: '/* eslint-disable */' + '\n' + '// @ts-nocheck'
+        }
+      }, 'typescript', 'typescript-operations', 'typescript-graphql-request'],
     },
   },
 };
 
 export default config;
+
+
+
