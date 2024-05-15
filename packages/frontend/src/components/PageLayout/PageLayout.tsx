8ad69791806693742ac3b8c5c427a79cf610923f
Frank Lyder Bredland <fl@nyggen.com>
2024-05-15 11:14:47 +0200

    feat(frontend): Use `leva` for debug features

--- packages/frontend/src/components/PageLayout/PageLayout.tsx
+++ packages/frontend/src/components/PageLayout/PageLayout.tsx
@@ -5,10 +5,10 @@ import { Outlet, useLocation } from 'react-router-dom';
 import { Footer, Header, Sidebar } from '..';
 import { fetchDialogByIdExample, fetchHelloWorld, fetchParties, fetchProfile } from '../../api/queries.ts';
 import { useAuthenticated } from '../../auth';
-import { FeatureFlagKeys, useFeatureFlag } from '../../featureFlags';
 import { getSearchStringFromQueryParams } from '../../pages/Inbox/Inbox';
 import { Snackbar } from '../Snackbar/Snackbar.tsx';
 import styles from './pageLayout.module.css';
+import { Leva, useControls, button } from 'leva';
 
 export const useUpdateOnLocationChange = (fn: () => void) => {
   const location = useLocation();
@@ -21,74 +21,46 @@ export const useParties = () => useQuery<PartiesQuery>('parties', fetchParties);
 
 export const PageLayout: React.FC = () => {
   const queryClient = useQueryClient();
-  const [companyName, setCompanyName] = React.useState<string>('Aker Solutions AS');
-  const isCompany = !!companyName;
-  const isDebugHeaderScreenEnabled = useFeatureFlag<boolean>(FeatureFlagKeys.EnableDebugHeaderScreen);
   const { data: partiesData } = useParties();
   const defaultParty = (partiesData?.parties ?? []).find((party) => party.partyType === 'Person')?.party;
   console.log('Default party uri used for fetching dialogs: ', defaultParty);
+
+	const { isCompany } = useControls({
+			isCompany: false,
+			helloWorld: button(async () => {
+				const response = await fetchHelloWorld();
+				console.log(response);
+			}),
+			fetchBtn: button(async () => {
+				const profile = await fetchProfile();
+				console.log(profile);
+			}),
+			testProxyBtn: button(async () => {
+				try {
+					const d = await fetchDialogByIdExample('14f18e01-7ed5-0272-a810-a5683df6c64d');
+					console.log(d.dialog?.status);
+				} catch(error) {
+					console.error('Error:', error);
+				}
+			}),
+			logoutBtn: button(()=> {
+				(window as Window).location = `/api/logout`;
+			}),
+	});
 
   useAuthenticated();
   useUpdateOnLocationChange(() => getSearchStringFromQueryParams(queryClient));
 
   return (
     <div className={isCompany ? `isCompany` : ''}>
-      {isDebugHeaderScreenEnabled && (
-        <section>
-          <button type="button" onClick={() => setCompanyName(companyName !== '' ? '' : 'Aker Solutions AS')}>
-            User/Company Switch
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              fetchHelloWorld().then((response) => {
-                console.log(response);
-              });
-            }}
-          >
-            Hello World
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              fetchProfile().then((profile) => {
-                console.log(profile);
-              });
-            }}
-          >
-            Fetch
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              fetchDialogByIdExample('14f18e01-7ed5-0272-a810-a5683df6c64d')
-                .then((d) => {
-                  console.log(d.dialog?.status);
-                })
-                .catch((error) => {
-                  console.error('Error:', error);
-                });
-            }}
-          >
-            Test proxy
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              (window as Window).location = `/api/logout`;
-            }}
-          >
-            Logout
-          </button>
-        </section>
-      )}
       <div className={styles.pageLayout}>
-        <Header name="John Doe" companyName={companyName} />
-        <Sidebar isCompany={!!companyName} />
+        <Header name="John Doe" companyName={isCompany ? 'ACME Corp' : ''} />
+        <Sidebar isCompany={isCompany} />
         <Outlet />
         <Footer />
       </div>
       <Snackbar />
+			<Leva />
     </div>
   );
 };
