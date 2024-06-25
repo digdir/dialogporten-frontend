7162cd052141f184e90ebc7eb47d837e2e17ad24
Frank Lyder Bredland <fl@nyggen.com>
2024-06-25 15:48:36 +0200

    fix(frontend): remove some unused stuff from api/queries

--- packages/frontend/src/api/queries.ts
+++ packages/frontend/src/api/queries.ts
@@ -1,17 +1,20 @@
+import {
+  type CreateSavedSearchMutation,
+  type DeleteSavedSearchMutation,
+  type SavedSearchInput,
+  type SavedSearchesQuery,
+  type UpdateSavedSearchMutation,
+  getSdk,
+} from 'bff-types-generated';
 import { GraphQLClient } from 'graphql-request';
-import { dialogs } from '../mocks/dialogs.tsx';
-import { HelloQuery, getSdk, SavedSearchesQuery, DeleteSavedSearchMutation, UpdateSavedSearchMutation, CreateSavedSearchMutation, SavedSearchInput } from 'bff-types-generated';
 
-const graphQLEndpoint = '/api/graphql';
-const graphQLClient = new GraphQLClient(graphQLEndpoint, { credentials: 'include' });
-export const graphQLSDK = getSdk(graphQLClient);
+export const graphQLSDK = getSdk(new GraphQLClient('/api/graphql', { credentials: 'include' }));
 
 export const profile = graphQLSDK.profile;
-export const getDialogs = (): Promise<typeof dialogs> => fetch('/dialogs').then((resp) => resp.json());
-
-export const fetchHelloWorld = (): Promise<HelloQuery> => graphQLSDK.hello();
-
 export const fetchSavedSearches = (): Promise<SavedSearchesQuery> => graphQLSDK.savedSearches();
-export const deleteSavedSearch = (id: number): Promise<DeleteSavedSearchMutation> => graphQLSDK.DeleteSavedSearch({ id });
-export const updateSavedSearch = (id: number, name: string): Promise<UpdateSavedSearchMutation> => graphQLSDK.UpdateSavedSearch({ id, name });
-export const createSavedSearch = (name: string, data: SavedSearchInput): Promise<CreateSavedSearchMutation> => graphQLSDK.CreateSavedSearch({ name, data });
+export const deleteSavedSearch = (id: number): Promise<DeleteSavedSearchMutation> =>
+  graphQLSDK.DeleteSavedSearch({ id });
+export const updateSavedSearch = (id: number, name: string): Promise<UpdateSavedSearchMutation> =>
+  graphQLSDK.UpdateSavedSearch({ id, name });
+export const createSavedSearch = (name: string, data: SavedSearchInput): Promise<CreateSavedSearchMutation> =>
+  graphQLSDK.CreateSavedSearch({ name, data });
