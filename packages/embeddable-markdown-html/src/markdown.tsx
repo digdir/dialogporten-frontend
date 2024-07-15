8822b33fa56761b8f62ae2d2ddc28d8441d41f0c
Frank Lyder Bredland <fl@nyggen.com>
2024-07-15 12:26:42 +0200

    feat(frontend): embeddable content
    - Storybook stories for embedable content
    
    - Parse field and use <Markdown> in frontend

--- packages/embeddable-markdown-html/src/markdown.tsx
+++ packages/embeddable-markdown-html/src/markdown.tsx
@@ -2,6 +2,7 @@ import type { Options as RemarkRehypeOptions } from 'mdast-util-to-hast';
 import { type ReactElement, useEffect, useState } from 'react';
 import * as prod from 'react/jsx-runtime';
 import rehypeReact from 'rehype-react';
+import rehypeSanitize from 'rehype-sanitize';
 import remarkParse, { type Options as RemarkParseOptions } from 'remark-parse';
 import remarkToRehype from 'remark-rehype';
 import { unified } from 'unified';
@@ -15,11 +16,11 @@ export const Markdown: React.FC<{
 }> = ({ children, onError = () => {} }) => {
   const [reactContent, setReactContent] = useState<ReactElement | null>(null);
 
-  // TODO: Could this be a useMemo call?
   useEffect(() => {
     unified()
       .use(remarkParse, {} as RemarkParseOptions)
       .use(remarkToRehype, {} as RemarkRehypeOptions)
+      .use(rehypeSanitize)
       .use(rehypeReact, production)
       .process(children)
       .then((vfile) => setReactContent(vfile.result as ReactElement))
