e2f5a3e036dd23ee5197b993ddf93d5d5ace85a6
Alexander Midteide <144693023+alexdigdir@users.noreply.github.com>
2024-07-17 12:46:38 +0200

    fix: Mobile design implementation of inbox and menu bar (#823)

--- packages/frontend/src/components/MenuBar/UserInfo.tsx
+++ packages/frontend/src/components/MenuBar/UserInfo.tsx
@@ -2,6 +2,7 @@ import { ChevronRightIcon } from '@navikt/aksel-icons';
 import cx from 'classnames';
 import { useTranslation } from 'react-i18next';
 import { Avatar } from '../Avatar';
+import { MenuItem } from './MenuItem';
 import { toTitleCase } from './NavigationDropdownMenu';
 import styles from './userInfo.module.css';
 
@@ -16,24 +17,37 @@ export const UserInfo = ({ name, companyName, onClick }: UserInfoProps) => {
   const companyNameTitleCase = toTitleCase(companyName);
   const nameTitleCase = toTitleCase(name);
   return (
-    <div
-      className={styles.menuItem}
-      onClick={onClick}
-      onKeyDown={(e) => e.key === 'Enter' && onClick()}
-      role="button"
-      tabIndex={0}
-    >
-      <div className={cx(styles.menuColumn, styles.menuColumnLeft)} title={name}>
-        <Avatar name={name} companyName={companyNameTitleCase} />
-        <div>
-          <div className={styles.primaryName}>{companyNameTitleCase || nameTitleCase}</div>
-          <div className={styles.secondaryName}>{companyNameTitleCase ? nameTitleCase : t('word.private')}</div>
+    <MenuItem
+      leftContent={
+        <div
+          className={cx(styles.menuColumn, styles.menuColumnLeft)}
+          title={name}
+          onClick={onClick}
+          onKeyUp={onClick}
+          role="button"
+          tabIndex={0}
+        >
+          <Avatar name={name} companyName={companyNameTitleCase} />
+          <div>
+            <div className={styles.primaryName}>{companyNameTitleCase || nameTitleCase}</div>
+            <div className={styles.secondaryName}>{companyNameTitleCase ? nameTitleCase : t('word.private')}</div>
+          </div>
         </div>
-      </div>
-      <div className={cx(styles.menuColumn, styles.menuColumnRight)}>
-        <span className={styles.wordChange}>{t('word.change')}</span>
-        <ChevronRightIcon className={styles.arrowIcon} />
-      </div>
-    </div>
+      }
+      rightContent={
+        <div
+          className={cx(styles.menuColumn, styles.menuColumnRight)}
+          onClick={onClick}
+          onKeyUp={onClick}
+          role="button"
+          tabIndex={0}
+        >
+          <span className={styles.wordChange}>{t('word.change')}</span>
+          <ChevronRightIcon className={styles.arrowIcon} />
+        </div>
+      }
+      isWhiteBackground
+      fullWidth
+    />
   );
 };
