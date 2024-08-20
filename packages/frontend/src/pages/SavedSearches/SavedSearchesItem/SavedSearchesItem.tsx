a4f2b7e8d06e8328bdf4d4cfc04c0ecb9c3ddeaa
Alexander Midteide <144693023+alexdigdir@users.noreply.github.com>
2024-08-20 14:18:08 +0200

    feat: Fixed Saved Search on mobile and updated according to new Figma design (#984)

--- packages/frontend/src/pages/SavedSearches/SavedSearchesItem/SavedSearchesItem.tsx
+++ packages/frontend/src/pages/SavedSearches/SavedSearchesItem/SavedSearchesItem.tsx
@@ -1,6 +1,7 @@
 import type { SavedSearchesFieldsFragment } from 'bff-types-generated';
 import { useTranslation } from 'react-i18next';
 import { HorizontalLine } from '../../../components';
+import { PlusIcon } from '../../../components/Icons/PlusIcon/PlusIcon.tsx';
 import SearchFilterTag from '../SearchFilterTag/SearchFilterTag.tsx';
 import styles from './savedSearchesItem.module.css';
 
@@ -12,6 +13,11 @@ interface SavedSearchesItemProps {
 
 export const SavedSearchesItem = ({ savedSearch, actionPanel, isLast }: SavedSearchesItemProps) => {
   if (!savedSearch?.data) return null;
+  const { searchString, filters, fromView } = savedSearch.data;
+  const queryParams = new URLSearchParams({
+    ...(searchString && { search: searchString }),
+    ...(filters?.length && { filters: JSON.stringify(filters) }),
+  });
   const searchData = savedSearch.data;
   const { t } = useTranslation();
 
@@ -19,8 +25,12 @@ export const SavedSearchesItem = ({ savedSearch, actionPanel, isLast }: SavedSea
     return (
       <>
         <div className={styles.savedSearchItem} key={savedSearch.id}>
-          <div className={styles.searchDetails}>{savedSearch.name}</div>
-          {actionPanel}
+          <div className={styles.searchDetails}>
+            <a href={`${fromView}?${queryParams.toString()}`} className={styles.goToSavedSearchLink}>
+              {savedSearch.name}
+            </a>
+          </div>
+          <div>{actionPanel}</div>
         </div>
         {!isLast && <HorizontalLine />}
       </>
@@ -34,21 +44,25 @@ export const SavedSearchesItem = ({ savedSearch, actionPanel, isLast }: SavedSea
     <>
       <div className={styles.savedSearchItem} key={savedSearch.id}>
         <div className={styles.searchDetails}>
-          <span>{searchData?.fromView && `In ${getRouteName(searchData.fromView)}:  `}</span>
-          <span className={styles.searchString}>{searchData?.searchString && `«${searchData.searchString}»`}</span>
-          {searchData?.searchString && `${searchData.filters?.length ? ' + ' : ''}`}
-          {searchData?.filters?.map((search, index) => {
-            return (
-              <SearchFilterTag
-                key={`${search?.id}${index}`}
-                searchId={search?.id}
-                searchValue={search?.value}
-                index={index}
-              />
-            );
-          })}
+          <a href={`${fromView}?${queryParams.toString()}`} className={styles.goToSavedSearchLink}>
+            <span className={styles.searchDetailsTitle}>
+              {searchData?.fromView && `${t('word.in')} ${getRouteName(searchData.fromView)}:  `}
+            </span>
+            <span className={styles.searchString}>{searchData?.searchString && `«${searchData.searchString}»`}</span>
+            {searchData?.searchString && searchData.filters?.length ? <PlusIcon /> : ''}
+            {searchData?.filters?.map((search, index) => {
+              return (
+                <SearchFilterTag
+                  key={`${search?.id}${index}`}
+                  searchId={search?.id}
+                  searchValue={search?.value}
+                  isLastItem={searchData?.filters?.length === index + 1}
+                />
+              );
+            })}
+          </a>
         </div>
-        {actionPanel}
+        <div className={styles.actionPanel}>{actionPanel}</div>
       </div>
       {!isLast && <HorizontalLine />}
     </>
