67fbc65193a65bd861ac1fecfe3a042b6f9e6b2d
Sean Erik Scully <sean.erik.scully@gmail.com>
2024-08-23 21:04:09 +0200

    feat: display embeddable markdown content in inbox detail
    fix

--- packages/embeddable-markdown-html/src/markdown.tsx
+++ packages/embeddable-markdown-html/src/markdown.tsx
@@ -10,12 +10,16 @@ import { unified } from 'unified';
 // @ts-expect-error: the react types are missing.
 const production = { Fragment: prod.Fragment, jsx: prod.jsx, jsxs: prod.jsxs };
 
-export const Markdown: React.FC<{
-  children: string;
-  onError?: (err: Error) => void;
-}> = ({ children, onError = () => {} }) => {
+export const Markdown: ({
+  children,
+  onError,
+}: { children: string; onError?: (error: unknown) => void }) => ReactElement | null = ({
+  children,
+  onError = () => {},
+}) => {
   const [reactContent, setReactContent] = useState<ReactElement | null>(null);
 
+  // biome-ignore lint/correctness/useExhaustiveDependencies: Full control of what triggers this code is needed
   useEffect(() => {
     unified()
       .use(remarkParse, {} as RemarkParseOptions)
@@ -24,8 +28,8 @@ export const Markdown: React.FC<{
       .use(rehypeReact, production)
       .process(children)
       .then((vfile) => setReactContent(vfile.result as ReactElement))
-      .catch((e) => onError(e));
-  }, [children, onError]);
+      .catch(onError);
+  }, [children]);
 
   return reactContent;
 };
