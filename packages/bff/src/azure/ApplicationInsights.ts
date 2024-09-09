9b7074c7959084c130d3885124d5da313d92827b
Are Almaas <arealmaas@gmail.com>
2024-09-09 19:35:50 +0200

    fix(bff): convert from application insights to open telemetry

--- packages/bff/src/azure/ApplicationInsights.ts
+++ packages/bff/src/azure/ApplicationInsights.ts
@@ -1,4 +1,5 @@
-import * as appInsights from 'applicationinsights';
+import { useAzureMonitor } from '@azure/monitor-opentelemetry';
+import { HttpInstrumentationConfig } from "@opentelemetry/instrumentation-http";
 import config from '../config.ts';
 
 const { applicationInsights } = config;
@@ -7,23 +8,47 @@ export const initialize = () => {
   const { connectionString } = applicationInsights;
   if (!connectionString) {
     const errorMsg =
-      'Unable to initialize Application Insights: Application Insights enabled, but connection string is missing.';
+      'Unable to initialize Application Insights Monitor: Application Insights enabled, but connection string is missing.';
     console.error(errorMsg);
     throw new Error(errorMsg);
   }
 
   try {
-    appInsights
-      .setup(connectionString)
-      .setAutoDependencyCorrelation(true)
-      .setAutoCollectRequests(true)
-      .setAutoCollectPerformance(true, true)
-      .setAutoCollectExceptions(true)
-      .setAutoCollectDependencies(true)
-      .setAutoCollectConsole(true, true)
-      .setUseDiskRetryCaching(true)
-      .setSendLiveMetrics(false)
-      .start();
+
+    // Filter using HTTP instrumentation configuration
+    const httpInstrumentationConfig: HttpInstrumentationConfig = {
+      enabled: true,
+      ignoreIncomingRequestHook: (request: IncomingMessage) => {
+          // Ignore OPTIONS incoming requests
+          if (request.method === 'OPTIONS') {
+              return true;
+          }
+          return false;
+      },
+      ignoreOutgoingRequestHook: (options: RequestOptions) => {
+          // Ignore outgoing requests with /test path
+          if (options.path === '/test') {
+              return true;
+          }
+          return false;
+      }
+  };
+
+    useAzureMonitor({
+      azureMonitorExporterOptions: {
+        connectionString,
+      },
+      instrumentationOptions: {
+        // Custom HTTP Instrumentation Configuration
+        http: httpInstrumentationConfig,
+        azureSdk: { enabled: true },
+        mongoDb: { enabled: true },
+        mySql: { enabled: true },
+        postgreSql: { enabled: true },
+        redis: { enabled: true },
+        redis4: { enabled: true },
+    },
+    });
   } catch (error) {
     console.error('Error initializing Application Insights:', error);
     throw error;
