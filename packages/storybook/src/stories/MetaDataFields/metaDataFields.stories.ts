7dec6bb560bb8ac86f71e309b5836c3e76af2996
Alexander Midteide <alexander.midteide@digdir.no>
2024-09-10 12:59:10 +0200

    feat: Added progress bar and updated storybook stories

--- packages/storybook/src/stories/MetaDataFields/metaDataFields.stories.ts
+++ packages/storybook/src/stories/MetaDataFields/metaDataFields.stories.ts
@@ -13,9 +13,13 @@ export default {
 export const Default: StoryObj<typeof MetaDataFields> = {
   args: {
     metaFields: [
-      { type: 'status', label: 'Status' },
-      { type: 'timestamp', label: 'Timestamp' },
-      { type: 'attachment', label: 'Attachment here' },
+      { type: 'status_completed', label: 'Status: Completed' },
+      { type: 'status_inprogress', label: 'Status: In Progress' },
+      { type: 'status_sent', label: 'Status: Sent' },
+      { type: 'status_requires_attention', label: 'Status: Requires Attention' },
+      { type: 'status_draft', label: 'Status: Draft' },
+      { type: 'timestamp', label: '5. september 15:40' },
+      { type: 'attachment', label: 'Vedlegg her' },
       {
         type: 'seenBy',
         label: 'Seen By You',
