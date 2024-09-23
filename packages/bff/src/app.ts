4c0f687e84f6cf6132112c3b6257bc15306b62c6
Are Almaas <arealmaas@gmail.com>
2024-09-23 21:07:57 +0200

    fix(bff): ensure instrumentation is initialized before starting the app (#1118)

--- packages/bff/src/app.ts
+++ packages/bff/src/app.ts
@@ -1,15 +1,17 @@
 #!/usr/bin/env -S node --no-warnings=ExperimentalWarning --loader ts-node/esm
-import './instrumentation.ts';
 import 'reflect-metadata';
 import { logger } from '@digdir/dialogporten-node-logger';
 import config from './config.ts';
-import { runMigrationApp } from './run-migration.ts';
-import startServer from './server.ts';
+import './instrumentation.ts';
 
 try {
+  // There is a race-condition where fastify, redis and postgres is booted before the instrumentation is ready.
+  // By starting the app or migration in a function we avoid the imports being hoisted, and we ensure that the instrumentation is ready before the app is started.
   if (config.migrationRun) {
+    const { runMigrationApp } = await import('./run-migration.ts');
     await runMigrationApp();
   } else {
+    const { default: startServer } = await import('./server.ts');
     await startServer();
   }
 } catch (error) {
