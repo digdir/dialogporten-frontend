8cde269723dd624d79fa14a7ee7a9fff50031b29
Are Almaas <arealmaas@gmail.com>
2024-09-23 16:13:36 +0200

    cleanup

--- packages/bff/src/app.ts
+++ packages/bff/src/app.ts
@@ -7,8 +7,8 @@ import './instrumentation.ts';
 try {
   // There is a race-condition where fastify, redis and postgres is booted before the instrumentation is ready.
   // By starting the app or migration in a function we avoid the imports being hoisted, and we ensure that the instrumentation is ready before the app is started.
-  const { runMigrationApp } = await import('./run-migration.ts');
   if (config.migrationRun) {
+    const { runMigrationApp } = await import('./run-migration.ts');
     await runMigrationApp();
   } else {
     const { default: startServer } = await import('./server.ts');
