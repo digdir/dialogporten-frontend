e21b27098588fb31b80d899b6b3e80168cba7168
Sean Erik Scully <sean.erik.scully@gmail.com>
2024-09-27 09:13:35 +0200

    draft: beta banner for release

---
+++ packages/frontend/src/components/BetaBanner/BetaBanner.tsx
@@ -0,0 +1,34 @@
+import { InformationSquareIcon, XMarkIcon } from '@navikt/aksel-icons';
+import styles from './betaBanner.module.css';
+
+import { Button } from '@digdir/designsystemet-react';
+import { useState } from 'react';
+
+export const BetaBanner = () => {
+  const betaBannerKey = 'arbeidsflate:show_beta_banner';
+  const isPreviouslyDismissed = localStorage.getItem(betaBannerKey) === 'true';
+  const [showBetaBanner, setShowBetaBanner] = useState<boolean>(!isPreviouslyDismissed);
+
+  const handleClick = () => {
+    setShowBetaBanner(false);
+    localStorage.setItem(betaBannerKey, 'true');
+  };
+
+  if (!showBetaBanner) {
+    return null;
+  }
+
+  return (
+    <section className={styles.betaBanner}>
+      <div className={styles.betaBannerTitle}>
+        <InformationSquareIcon className={styles.infoIcon} />
+        <span>
+          Du ser nå på en beta-versjon av nye Altinn Innboks. Innholdet du ser er ikke reelt og kun ment for demo.
+        </span>
+      </div>
+      <Button variant="tertiary" onClick={handleClick} type="button" className={styles.closeButton}>
+        <XMarkIcon className={styles.closeIcon} />
+      </Button>
+    </section>
+  );
+};
