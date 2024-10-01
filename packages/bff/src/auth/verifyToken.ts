4949c4cc261de0eda20452544267ce2ab9a1cb6f
Sean Erik Scully <sean.erik.scully@gmail.com>
2024-10-01 11:53:06 +0200

    fix: improvements to auth / refresh flow

--- packages/bff/src/auth/verifyToken.ts
+++ packages/bff/src/auth/verifyToken.ts
@@ -1,55 +1,50 @@
 import { logger } from '@digdir/dialogporten-node-logger';
 import axios from 'axios';
-import type { FastifyPluginAsync, FastifyRequest, IdPortenUpdatedToken } from 'fastify';
+import type { FastifyPluginAsync, FastifyReply, FastifyRequest, IdPortenUpdatedToken } from 'fastify';
 import fp from 'fastify-plugin';
 import config from '../config.ts';
-import type { SessionStorageToken } from './oidc.ts';
+import { type SessionStorageToken, handleAuthRequest, handleLogout } from './oidc.ts';
 
-const refreshAllTokens = async (request: FastifyRequest) => {
+export const refreshToken = async (request: FastifyRequest) => {
   const token: SessionStorageToken | undefined = request.session.get('token');
+  const { client_id, client_secret, oidc_url } = config;
 
   if (!token) {
     return;
   }
 
-  const tokenEndpoint = `https://${config.oidc_url}/token`;
-  const basicAuthString = `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`;
+  const tokenEndpoint = `https://${oidc_url}/token`;
+  const basicAuthString = `${client_id}:${client_secret}`;
   const authEncoded = `Basic ${Buffer.from(basicAuthString).toString('base64')}`;
 
-  try {
-    const refreshResponse = await axios.post(
-      tokenEndpoint,
-      {
-        grant_type: 'refresh_token',
-        refresh_token: token.refresh_token,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          Authorization: authEncoded,
-        },
-      },
-    );
+  const body = new URLSearchParams();
+  body.append('grant_type', 'refresh_token');
+  body.append('refresh_token', token.refresh_token);
 
-    const updatedToken: IdPortenUpdatedToken = refreshResponse?.data;
+  const refreshResponse = await axios.post(tokenEndpoint, body, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: authEncoded,
+    },
+  });
 
-    if (updatedToken) {
-      const refreshTokenExpiresAt = new Date(Date.now() + updatedToken.refresh_token_expires_in * 1000).toISOString();
-      const accessTokenExpiresAt = new Date(Date.now() + updatedToken.expires_in * 1000).toISOString();
+  const updatedToken: IdPortenUpdatedToken = refreshResponse?.data;
 
-      const updatedSessionStorageToken = {
-        ...token,
-        access_token: updatedToken.access_token,
-        refresh_token_expires_at: refreshTokenExpiresAt,
-        refresh_token: updatedToken.refresh_token,
-        access_token_expires_at: accessTokenExpiresAt,
-      };
+  if (updatedToken) {
+    const refreshTokenExpiresAt = new Date(Date.now() + updatedToken.refresh_token_expires_in * 1000).toISOString();
+    const accessTokenExpiresAt = new Date(Date.now() + updatedToken.expires_in * 1000).toISOString();
 
-      request.session.set('token', updatedSessionStorageToken);
-    }
-  } catch (error) {
-    logger.error(error, 'refreshAllTokens: Error during token refresh');
-    throw error;
+    const updatedSessionStorageToken: SessionStorageToken = {
+      id_token: token.id_token, // id_token is not returned in the refresh response
+      scope: token.scope, // scope will not change
+      access_token: updatedToken.access_token,
+      refresh_token: updatedToken.refresh_token,
+      refresh_token_expires_at: refreshTokenExpiresAt,
+      access_token_expires_at: accessTokenExpiresAt,
+      tokenUpdatedAt: new Date().toISOString(),
+    };
+
+    request.session.set('token', updatedSessionStorageToken);
   }
 };
 
@@ -71,43 +66,41 @@ const getIsTokenValid = async (request: FastifyRequest, allowTokenRefresh: boole
   const now = new Date();
   const accessTokenExpiresAt = new Date(token.access_token_expires_at);
   const isRefreshTokenValid = new Date(token.refresh_token_expires_at) > now;
-  const accessTokenWillExpireNextMinute = accessTokenExpiresAt.getTime() - now.getTime() < 60 * 1000;
+  // Check if the access token expires in less than 60 seconds for now
+  const accessTokenExpiresSoon = accessTokenExpiresAt.getTime() - now.getTime() < 60 * 1000;
   const isAccessTokenValid = accessTokenExpiresAt > now;
 
-  if (accessTokenWillExpireNextMinute && allowTokenRefresh && isRefreshTokenValid) {
+  if (accessTokenExpiresSoon && allowTokenRefresh && isRefreshTokenValid) {
     try {
-      await refreshAllTokens(request);
-      return true;
+      await refreshToken(request);
+      // Ensure that the token has been updated and valid after the refresh
+      const updatedToken: SessionStorageToken | undefined = request.session.get('token');
+      const updatedAccessTokenExpiresAt = new Date(updatedToken?.access_token_expires_at || '');
+      return updatedAccessTokenExpiresAt > now;
     } catch (error) {
       logger.error(error, 'Unable to refresh token');
     }
   }
 
-  if (isAccessTokenValid) {
-    return true;
-  }
-
-  if (!isRefreshTokenValid) {
-    return false;
-  }
-
-  if (!allowTokenRefresh) {
-    return false;
-  }
-
-  try {
-    await refreshAllTokens(request);
-    return true;
-  } catch (e) {
-    return false;
-  }
+  return isAccessTokenValid;
 };
 
 const plugin: FastifyPluginAsync = async (fastify, _) => {
   fastify.decorate('verifyToken', (allowTokenRefresh: boolean) => {
-    return async (request: FastifyRequest) => {
+    return async (request: FastifyRequest, reply: FastifyReply) => {
       try {
         request.tokenIsValid = await getIsTokenValid(request, allowTokenRefresh);
+
+        if (!request.tokenIsValid && allowTokenRefresh) {
+          // No point in trying to refresh the token again
+          const token: SessionStorageToken | undefined = request.session.get('token');
+          if (token) {
+            await handleAuthRequest(request, reply, fastify);
+            request.tokenIsValid = true;
+          } else {
+            await handleLogout(request, reply);
+          }
+        }
       } catch (e) {
         logger.error(e, 'Unable to verify token');
         request.tokenIsValid = false;
