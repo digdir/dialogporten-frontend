1dc8932755e4085db025c64b7923bd08d5740523
Sean Erik Scully <sean.erik.scully@gmail.com>
2024-10-17 14:06:24 +0200

    fix: allow refreshing of access token when graphql request is executed through graphiql IDE

--- packages/bff/src/graphql/api.ts
+++ packages/bff/src/graphql/api.ts
@@ -1,7 +1,7 @@
 import { stitchSchemas } from '@graphql-tools/stitch';
 import type { AsyncExecutor } from '@graphql-tools/utils';
 import axios from 'axios';
-import type { FastifyPluginAsync } from 'fastify';
+import type { FastifyPluginAsync, FastifyRequest } from 'fastify';
 import fp from 'fastify-plugin';
 import { print } from 'graphql';
 import { createHandler } from 'graphql-http/lib/use/fastify';
@@ -43,10 +43,20 @@ const plugin: FastifyPluginAsync = async (fastify) => {
     },
   });
 
-  fastify.post('/api/graphql', { preHandler: fastify.verifyToken(false) }, async (request, reply) => {
-    await handler.call(fastify, request, reply);
-    return reply;
-  });
+  fastify.post(
+    '/api/graphql',
+    {
+      preHandler: (request: FastifyRequest, reply, callback) => {
+        /* Allow refresh to keep access token alive if request is from GraphQL IDE */
+        const shouldVerifyToken = request.headers.referer?.includes('/api/graphiql') ?? false;
+        return fastify.verifyToken(shouldVerifyToken)(request, reply, callback);
+      },
+    },
+    async (request, reply) => {
+      await handler.call(fastify, request, reply);
+      return reply;
+    },
+  );
 };
 
 export default fp(plugin, {
