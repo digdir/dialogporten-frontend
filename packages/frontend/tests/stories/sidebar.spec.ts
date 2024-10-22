5cfdfe950143719b75d00e138e11c70bc63610d1
Alexander Midteide <144693023+alexdigdir@users.noreply.github.com>
2024-10-22 12:16:03 +0200

    chore: Playwright tests for sidebar component (#1274)

---
+++ packages/frontend/tests/stories/sidebar.spec.ts
@@ -0,0 +1,20 @@
+import { expect, test } from '@playwright/test';
+import { appURL } from '../';
+
+test.describe('Sidebar menu', () => {
+  test('Checking all items in sidebar', async ({ page }) => {
+    await page.goto(appURL);
+    await page.getByRole('link', { name: 'Utkast' }).click();
+    await expect(page.getByRole('heading', { name: 'utkast' })).toBeVisible();
+    await page.getByRole('link', { name: 'Sendt' }).click();
+    await expect(page.getByRole('heading', { name: 'sendt' })).toBeVisible();
+    await page.getByRole('link', { name: 'Lagrede søk' }).click();
+    await expect(page.getByText('Du har ingen lagrede søk')).toBeVisible();
+    await page.getByRole('link', { name: 'Arkiv' }).click();
+    await expect(page.getByRole('heading', { name: 'Ingen meldinger' })).toBeVisible();
+    await page.getByRole('link', { name: 'Papirkurv' }).click();
+    await expect(page.getByRole('heading', { name: 'Ingen meldinger' })).toBeVisible();
+    await page.getByRole('link', { name: 'Innboks' }).click();
+    await expect(page.getByRole('link', { name: 'Melding om bortkjøring av sn' })).toBeVisible();
+  });
+});
