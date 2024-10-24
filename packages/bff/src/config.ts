9f2340b2ba3c946663eabc905b31379a86174e19
Mateusz Bacherycz <mbacherycz@icloud.com>
2024-10-24 14:16:54 +0200

    chore: change default value to show graphiql console to true

--- packages/bff/src/config.ts
+++ packages/bff/src/config.ts
@@ -21,7 +21,7 @@ const envVariables = z.object({
   MIGRATION_RUN: z.coerce.boolean().default(false),
   DIALOGPORTEN_URL: z.string().default('https://altinn-dev-api.azure-api.net/dialogporten/graphql'),
   CONTAINER_APP_REPLICA_NAME: z.string().default(''),
-  ENABLE_GRAPHIQL: z.coerce.boolean().default(false),
+  ENABLE_GRAPHIQL: z.coerce.boolean().default(true),
 });
 
 const env = envVariables.parse(process.env);
