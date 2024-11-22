9935b455f7a3648380028fd375a7376e71ae1597
Sean Erik Scully <sean.erik.scully@gmail.com>
2024-11-22 11:54:15 +0100

    fix: update dialogporten-schema to 1.38.x

--- packages/frontend/src/mocks/data/base/parties.ts
+++ packages/frontend/src/mocks/data/base/parties.ts
@@ -32,6 +32,7 @@ export const parties: PartyFieldsFragment[] = [
         isMainAdministrator: true,
         name: 'TESTBEDRIFT AS AVD SUB',
         isCurrentEndUser: false,
+        isDeleted: false,
       },
       {
         party: 'urn:altinn:organization:identifier-sub:2',
@@ -40,6 +41,7 @@ export const parties: PartyFieldsFragment[] = [
         isMainAdministrator: true,
         name: 'TESTBEDRIFT AS AVD OSLO',
         isCurrentEndUser: false,
+        isDeleted: false,
       },
     ],
     isAccessManager: true,
