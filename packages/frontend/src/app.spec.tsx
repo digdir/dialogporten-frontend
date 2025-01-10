28cfe8262e159721be61b3686a0e445fe12af9eb
Alexander Midteide <alexander.midteide@digdir.no>
2025-01-10 11:48:46 +0100

    Pr comments

--- packages/frontend/src/app.spec.tsx
+++ packages/frontend/src/app.spec.tsx
@@ -1,4 +1,4 @@
-import 'web-streams-polyfill/es6'; // Update this line
+import 'web-streams-polyfill/es6';
 import { beforeAll, describe, expect, it } from 'vitest';
 import { customRender } from '../utils/test-utils.tsx';
 import App from './App.tsx';
