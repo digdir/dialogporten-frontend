c765015a8bac2d024d47c5f11586e7f07d4d0468
Sean Erik Scully <sean.erik.scully@gmail.com>
2025-01-14 12:56:39 +0100

    chore: fix typeorm migration - blabla

---
+++ packages/bff/src/migrations/1736854855787-init.ts
@@ -0,0 +1,7 @@
+import type { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Init1736854855787 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
