0ca677a229a71a0bb771c211099ab0f6c1b7c069
Mateusz Marek Bacherycz <mbacherycz@icloud.com>
2025-01-24 09:36:15 +0100

    chore: Cover App routes with AxeBuilder in Playwright (#1719)

--- packages/frontend/tests/accessibility/axe.test.ts
+++ packages/frontend/tests/accessibility/axe.test.ts
@@ -1,13 +1,33 @@
-import AxeBuilder from '@axe-core/playwright';
-import { expect, test } from '@playwright/test';
+import { AxeBuilder } from '@axe-core/playwright';
+import { type Page, expect, test } from '@playwright/test';
 import { defaultAppURL } from '..';
 
+const WCAG_TAGS_CONFIG = ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'];
+const VIOLATION_FILTERS = ['critical', 'serious'];
+const expectWithFilterViolations = (violations) => {
+  const filteredViolations = violations.filter((violation) => VIOLATION_FILTERS.includes(violation.impact));
+  expect(filteredViolations).toEqual([]);
+};
+
+const testAccessibility = async (page: Page, url: string) => {
+  await page.goto(url);
+  const accessibilityScanResults = await new AxeBuilder({ page }).withTags(WCAG_TAGS_CONFIG).analyze();
+
+  expectWithFilterViolations(accessibilityScanResults.violations);
+};
+
 test.describe('Axe test', () => {
-  test('should not have any automatically detectable accessibility issues', async ({ page }) => {
-    await page.goto(defaultAppURL);
+  const testCases = [
+    { name: 'Inbox', path: '' },
+    { name: 'Drafts', path: '/drafts' },
+    { name: 'Sent', path: '/sent' },
+    { name: 'Archive', path: '/archive' },
+    { name: 'Bin', path: '/bin' },
+  ];
 
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
+  for (const { name, path } of testCases) {
+    test(`${name} - should not have any automatically detectable accessibility issues`, async ({ page }) => {
+      await testAccessibility(page, `${defaultAppURL}${path}`);
+    });
+  }
 });
