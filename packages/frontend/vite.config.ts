fa5f695f25e85a5c95d0ef98dc0902288b3a2cdd
Mateusz Bacherycz <mbacherycz@icloud.com>
2025-02-07 12:09:10 +0100

    fix: Log info correct app version

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import pkg from './package.json';
+import pkg from '../../package.json';
 
 // https://vitejs.dev/config/
 export default () => {
